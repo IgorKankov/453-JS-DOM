ab007983f75bcee39a1f30ce0c141064bd0e302d
Igor <Kankovigor@gmail.com>
2019-11-13 16:28:56 +0000

    js practised

---
+++ js/index.js
@@ -0,0 +1,45 @@
+if (Math.random() > 0.5) {
+    const link = document.querySelector('a');
+    link.href = 'http://programmingmentor.com';
+    link.textContent = 'Programming Mentor';
+}
+
+const sect = document.querySelector('section');
+const para = document.createElement('p');
+para.textContent = 'We hope you enjoyed the ride.';
+sect.appendChild(para);
+
+const text = document.createTextNode(' — the premier source for web development knowledge.');
+const linkPara = document.querySelector('p');
+linkPara.appendChild(text);
+
+sect.appendChild(linkPara.cloneNode(true));
+
+// old
+// linkPara.parentNode.removeChild(linkPara);
+linkPara.remove();
+para.style.color = 'white';
+para.style.backgroundColor = 'black';
+para.style.padding = '10px';
+para.style.width = '250px';
+para.style.textAlign = 'center';
+const clock = document.querySelector('.clock');
+setInterval(() => {
+    clock.innerHTML = new Date().toLocaleTimeString();
+}, 1000);
+
+const display = document.querySelector('.calc .display');
+document.querySelectorAll('.calc .digits button, .calc .opers button')
+    .forEach( button => button.addEventListener('click', digitOperPressed));
+
+function digitOperPressed(event) {
+    const btnText = event.target.innerText;
+    display.value += btnText;
+}
+
+document.querySelector('.calc .eq')
+    .addEventListener('click', eqPressed);
+
+function eqPressed() {
+    display.value = eval(display.value);
+}
